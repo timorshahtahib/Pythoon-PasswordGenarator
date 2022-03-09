7eaed23aa7d67f0818b5337cda882cc7b09200c4
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 14:49:32 +0330

    password generator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import string,random
 #listcomperhencive
 # nums=[num for num in range(10)]
 # print(nums)
@@ -6,6 +6,32 @@
 
 
 #odd number 
-nums=[num for num in range(10) if num%2==0] 
-print(nums)
+# nums=[num for num in range(10) if num%2==0] 
+# print(nums)
 
+
+while True:
+    len=int(input("input your password lenghts :   "))
+    chars=string.ascii_letters+string.digits+"@!#$%()"
+    # text=[]
+    # for i in range(len):
+    #     text.append(random.choice(chars))
+    # password="".join(text)
+    # print("your password is :  {}".format(password))
+    
+    passs="".join([random.choice(chars) for i in range(len)])
+    
+   
+    print("your password is :  {}".format(passs))
+    
+    while True:
+        
+        repet_again=input("dow you wanto repet aganin (Y/n)").lower()
+        
+        if repet_again=='n' or repet_again=='y':
+            break
+        
+
+    if repet_again=='n':
+        break
+    
