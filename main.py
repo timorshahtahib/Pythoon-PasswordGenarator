8c26d5dd178fae71402cd0cf725020a3ed06b448
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-09 14:22:42 +0330

    odd number

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 
 #listcomperhencive
-nums=[num for num in range(10)]
+# nums=[num for num in range(10)]
+# print(nums)
+
+
+
+#odd number 
+nums=[num for num in range(10) if num%2==0] 
 print(nums)
+
